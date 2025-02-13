c22c3620ceac732b33b8cb4ce5608700de45387c
Sendiko <sarangtawon897@gmail.com>
2025-02-13 12:20:57 +0700

    Feat: Add `getData()` function to `MainActivity`
    - Add `getData()` to get list of `Hewan`.
    - Create list of `Hewan` with their name and drawable resource.

--- app/src/main/java/com/sendiko0084/about_me/MainActivity.kt
+++ app/src/main/java/com/sendiko0084/about_me/MainActivity.kt
@@ -11,9 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.sendiko0084.about_me.model.Hewan
 import com.sendiko0084.about_me.ui.theme.Mobpro1Theme
-
 class MainActivity : ComponentActivity() {
+
+    private val data = getData()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,7 +26,41 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    private fun getData(): List<Hewan> {
+        return listOf(
+            Hewan(
+                nama = "Ayam",
+                imageResId = R.drawable.ayam
+            ),
+            Hewan(
+                nama = "Bebek",
+                imageResId = R.drawable.bebek
+            ),
+            Hewan(
+                nama = "Kambing",
+                imageResId = R.drawable.kambing
+            ),
+            Hewan(
+                nama = "Bebek",
+                imageResId = R.drawable.bebek
+            ),
+            Hewan(
+                nama = "Domba",
+                imageResId = R.drawable.domba
+            ),
+            Hewan(
+                nama = "Kambing",
+                imageResId = R.drawable.kambing
+            ),
+            Hewan(
+                nama = "Sapi",
+                imageResId = R.drawable.sapi
+            ),
+        )
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
